376d7fd65a230f084c03eb240f3f553038f970f1
hai <hilin2333@gmail.com>
2017-12-10 14:29:14 +0800

    fix contructor disappear

--- src/silencecorner/intellij/plugin/evo/GenerateEnumValueOf.java
+++ src/silencecorner/intellij/plugin/evo/GenerateEnumValueOf.java
@@ -29,7 +29,9 @@ public class GenerateEnumValueOf extends EditorAction {
         }
         PsiClass javaClass = util.getCurrentClass(editor);
         //设置enum可访问,且只有一个构造函数,且构造有至少一个参数
-        if (javaClass.isEnum() && javaClass.getConstructors().length == 1
+        if (javaClass.isEnum()
+                && javaClass.getConstructors() != null
+                && javaClass.getConstructors().length == 1
                 && javaClass.getConstructors()[0].getParameterList().getParameters().length > 0) {
             presentation.setVisible(true);
             return;
