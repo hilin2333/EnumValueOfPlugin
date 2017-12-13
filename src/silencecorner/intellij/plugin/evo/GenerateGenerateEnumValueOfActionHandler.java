81d47b30e15b5682932ba225c17bcaeecba188ed
linhai <hilin2333@gmail.com>
2017-12-14 00:32:20 +0800

    使用Enum.values()方式

--- src/silencecorner/intellij/plugin/evo/GenerateGenerateEnumValueOfActionHandler.java
+++ src/silencecorner/intellij/plugin/evo/GenerateGenerateEnumValueOfActionHandler.java
@@ -6,19 +6,12 @@ import com.intellij.openapi.editor.Caret;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.actionSystem.EditorWriteActionHandler;
 import com.intellij.openapi.ui.Messages;
-import com.intellij.openapi.util.Comparing;
 import com.intellij.psi.*;
 import com.intellij.psi.codeStyle.CodeStyleManager;
-import com.intellij.psi.util.PsiUtil;
 import com.intellij.psi.util.TypeConversionUtil;
-import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class GenerateGenerateEnumValueOfActionHandler extends EditorWriteActionHandler {
-    private static final Pattern pattern = Pattern.compile("\\(.*\\)");
     @Override
     public void executeWriteAction(final Editor editor, @Nullable Caret caret, final DataContext dataContext) {
 
@@ -28,83 +21,34 @@ public class GenerateGenerateEnumValueOfActionHandler extends EditorWriteActionH
 
         PsiClass clazz = util.getCurrentClass(editor);
         PsiParameter psiParameter = clazz.getConstructors()[0].getParameterList().getParameters()[0];
-        if (!check(psiParameter.getType())){
-            showMsg(editor);
-            return;
+        String strGetMthod = "get" + psiParameter.getName().substring(0,1).toUpperCase() + psiParameter.getName().substring(1);
+        if (clazz.findMethodsByName(strGetMthod,false).length < 0){
+            Messages.showMessageDialog(editor.getProject(),
+                    "use this plugin must have  a public get method!",
+                    String.format("the method %s not exist",strGetMthod),
+                    Messages.getWarningIcon());
         }
+
         //删除已存在valueOf方法
         PsiMethod[] psiMethods = clazz.findMethodsByName("valueOf",false);
         for (PsiMethod method : psiMethods){
             method.delete();
         }
         PsiMethod psiMethod = psiElementFactory.createMethodFromText("public static " + clazz.getName() +" valueOf(" + psiParameter.getType().getPresentableText() +  " " + psiParameter.getName() +") {}",null);
-        //创建switch case语句
-        String text = "switch (" + psiParameter.getName() + ") {}";
-        PsiSwitchStatement switchBlock = (PsiSwitchStatement)psiElementFactory.createStatementFromText(text, clazz);
-        int i = 0;
-        for (PsiField field : clazz.getFields()) {
-            if (field instanceof PsiEnumConstant) {
-                String fieldName = field.getName();
-                PsiExpression[] psiExpressions =  ((PsiEnumConstant) field).getArgumentList().getExpressions();
-                switchBlock.getBody().add(psiElementFactory.createStatementFromText(
-                        "case " + getCase(psiExpressions[0].getText()) + ":"
-                        ,
-                        null));
-                switchBlock.getBody().add(psiElementFactory.createStatementFromText(
-                        "return " +  fieldName + ";",
-                        null));
-
-            }
-
+        String compareText;
+        if (TypeConversionUtil.getTypeRank(psiParameter.getType()) <= TypeConversionUtil.INT_RANK) {
+            compareText = "== %s";
+        }else{
+            compareText = ".equal(%s)";
         }
-        switchBlock.getBody().add(psiElementFactory.createStatementFromText("default:", switchBlock));
-        switchBlock.getBody().add(psiElementFactory.createStatementFromText("throw new RuntimeException(\"invalid  " + psiParameter.getName() + " for enum " + clazz.getName() + "\");",switchBlock));
+        //创建foreach语句
+        String text = "for("+ clazz.getName() + " " + clazz.getName().toLowerCase() + ":" + clazz.getName() + ".values()){"
+                    + "if (" + clazz.getName().toLowerCase() + "." + strGetMthod + "()" + String.format(compareText,psiParameter.getName())  +"){return " + clazz.getName().toLowerCase() +";}}";
+        psiMethod.getBody().add(psiElementFactory.createStatementFromText(text, null));
+        psiMethod.getBody().add(psiElementFactory.createStatementFromText("return null;", null));
         CodeStyleManager styleManager = CodeStyleManager.getInstance(editor.getProject());
-        PsiElement psiElement = styleManager.reformat(switchBlock);
-        psiMethod.getBody().add(psiElement);
-        clazz.add(psiMethod);
-    }
-    public String getCase(String text){
-        Matcher m = pattern.matcher(text);
-        while (m.find()) {
-            return text.replace(m.group(0),"");
-        }
-        return text;
-    }
-    private enum SelectorKind {
-        INT, ENUM, STRING
-    }
+        PsiElement psiElement = styleManager.reformat(psiMethod);
 
-    private static SelectorKind getSwitchSelectorKind(@NotNull PsiType type) {
-        if (TypeConversionUtil.getTypeRank(type) <= TypeConversionUtil.INT_RANK) {
-            return SelectorKind.INT;//这里指定的是整形
-        }
-
-        PsiClass psiClass = PsiUtil.resolveClassInClassTypeOnly(type);
-        if (psiClass != null) {
-            if (psiClass.isEnum()) {
-                return SelectorKind.ENUM;
-            }
-            if (Comparing.strEqual(psiClass.getQualifiedName(), CommonClassNames.JAVA_LANG_STRING)) {
-                return SelectorKind.STRING;
-            }
-        }
-
-        return null;
-    }
-    private boolean check(PsiType psiType){
-        boolean flag = false;
-        for (SelectorKind kind : SelectorKind.values()){
-            if (kind.equals(getSwitchSelectorKind(psiType))){
-                flag = true;
-                break;
-            }
-        }
-        return flag;
-    }
-    private void  showMsg(Editor editor){
-        Messages.showMessageDialog(editor.getProject(),
-                "the first of enum constructor parameter can't be used for switch", "occur an error",
-                Messages.getWarningIcon());
+        clazz.add(psiElement);
     }
 }
